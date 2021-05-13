53391691af231d158d90df9fa6376f78f99536f6
iwelsh47 <8754457+iwelsh47@users.noreply.github.com>
2021-05-13 13:48:22 +1200

    Added cubic spline

---
+++ include/Shapely/cubic_spline.hpp
@@ -0,0 +1,13 @@
+//
+//  cubic_spline.hpp
+//  shapely_tester
+//
+//  Created by Ivan Welsh on 7/05/21.
+//
+
+#ifndef cubic_spline_hpp
+#define cubic_spline_hpp
+
+#include <stdio.h>
+
+#endif /* cubic_spline_hpp */
---
+++ src/cubic_spline.cpp
@@ -0,0 +1,8 @@
+//
+//  cubic_spline.cpp
+//  shapely_tester
+//
+//  Created by Ivan Welsh on 7/05/21.
+//
+
+#include "cubic_spline.hpp"
